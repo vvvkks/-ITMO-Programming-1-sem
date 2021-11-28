a524fef2170cdcc5e5a38bdfd5aecde00cd47af3
Vika <89826699+vvvkks@users.noreply.github.com>
2021-11-28 21:32:34 +0300

    Add files via upload

---
+++ main.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main() {
+    char a[100];
+    int b;
+    printf("Enter your line = ");
+    scanf("%s",&a);
+    printf("Enter your number = ");
+    scanf("%d",&b);
+    printf("%s\n%d\n",a,b);
+
+    return 0;
+}
