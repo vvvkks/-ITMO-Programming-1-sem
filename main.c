10291f294e084d8032450257d2b5c7be916df895
Vika <89826699+vvvkks@users.noreply.github.com>
2021-12-21 14:42:02 +0300

    Add files via upload

---
+++ main.c
@@ -0,0 +1,48 @@
+#include<stdlib.h>
+#include<stdio.h>
+#include<math.h>
+
+int main(){
+
+    enum music {classical, pop, rock, rap, newage, electronic};
+    enum music x;
+    x = rock;
+    printf("%d \n", x);
+
+
+    struct triangle {
+        int x1, y1, x2, y2, x3, y3;
+    };
+
+    struct triangle A = {1, 2, 3, 4, 5, 6};
+
+    float AB = sqrt(pow((A.x2 - A.x1),2) + pow((A.y2 - A.y1),2));
+    float BC = sqrt(pow((A.x3 - A.x2),2) + pow((A.y3 - A.y2),2));
+    float AC = sqrt(pow((A.x3 - A.x1),2) + pow((A.y3 - A.y1),2));
+    float P = AB + AC + BC;
+
+    printf("%d \n ", P);
+
+    union status{
+        struct{
+            unsigned Active: 1;
+            unsigned SDcard: 1;
+            unsigned CompactFlashcard: 1;
+            unsigned MemoryStickcard: 1;
+        }bits;
+        unsigned char data;
+    };
+
+    union status MyStatus;
+
+    printf("\n\n Enter a hex number:");
+    scanf("%x", &(MyStatus.data));
+    printf("\nCard-reader status - ");
+    printf("\nActive: %s", (MyStatus.bits.Active) ? "On" : "Off");
+    printf("\nSD card: %s", (MyStatus.bits.SDcard) ? "On" : "Off");
+    printf("\nCompact flash card: %s", (MyStatus.bits.CompactFlashcard) ? "On" : "Off");
+    printf("\nMemory stick card: %s", (MyStatus.bits.MemoryStickcard) ? "On" : "Off");
+    printf("\n\n");
+
+    return 0;
+}
